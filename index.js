258da4fa311951e3e628995f394bfd4ce0b00e49
Darkweld <Darkweld@Darkweld@users.noreply.github.com>
2017-05-04 05:47:17 +0000

    added trust_proxy to index.js

--- index.js
+++ index.js
@@ -5,13 +5,12 @@ app.use(express.static("css"));
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
-
-
+app.enable("trust proxy");
 
 
 app.get("/", function (req, res) {
     var obj = {ip: null, lang: null, os: null};
-    obj.ip = req.ip.slice(7);
+    obj.ip = req.ip;
     obj.lang = req.headers["accept-language"];
     obj.os = req.headers["user-agent"];
     obj.os = obj.os.slice(obj.os.indexOf("(") + 1, obj.os.indexOf(")"));
