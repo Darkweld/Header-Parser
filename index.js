fbc2da5c43cf74a8c1df020f0017d65c03d4623e
Darkweld <Darkweld@Darkweld@users.noreply.github.com>
2017-05-04 04:35:00 +0000

    fixed css path for heroku (hopefully)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static(__dirname, "/css"));
+app.use(express.static("css"));
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
